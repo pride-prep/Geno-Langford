1b9506434abe88c439c21b124ea2679e366f877b
HuhYaBoi <geno@my.prideprepschool.org>
2017-05-09 13:09:35 -0700

    REAL HW2

---
+++ HW2_GENO/HW2_GENO/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HW2_GENO
+//
+//  Created by Student on 5/9/17.
+//  Copyright © 2017 PRIDE Prep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Label1: UILabel!
+    
+    @IBOutlet weak var timesTable1: UIImageView!
+    
+    @IBAction func nextPage(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
